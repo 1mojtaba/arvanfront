6243f6e33e39e0b77216a70f3b264d71a2c7dfa5
1mojtaba <77581023+1mojtaba@users.noreply.github.com>
2023-05-22 23:56:51 +0330

    version2.0.0

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -126,7 +126,6 @@ function Dashboard() {
       </svg>
     </button>
   </div>
-
   <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
     <div class="hidden" id="dropdown" role="none">
       <a href="#" class="no-underline text-gray-700 block px-4 py-2 text-sm text-black" role="menuitem" tabindex="-1" id="menu-item-0">Edit</a>
